56232a317d5ff151e557cc78baa2fe73fc261159
Benjamin Abbitt <benjamin.abbitt@gmail.com>
2020-05-06 20:39:20 -0500

    configured logging, configuration libraries.  Projector should be (untested today) projecting into Mongo

--- src/main/java/name/benjaminabbitt/evented/Projector/ProjectorService.java
+++ src/main/java/name/benjaminabbitt/evented/Projector/ProjectorService.java
@@ -1,30 +1,29 @@
-package Projector;
+package name.benjaminabbitt.evented.Projector;
 
 import com.google.protobuf.Empty;
-import com.google.protobuf.InvalidProtocolBufferException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-import name.benjaminabbitt.evented.bookmarks.Bookmarks;
-import name.benjaminabbitt.evented.business.BusinessLogicGrpc;
+import name.benjaminabbitt.evented.Projector.Projector.BookmarkProjector;
 import name.benjaminabbitt.evented.core.Evented;
-import name.benjaminabbitt.evented.projector.ProjectorGrpc;
 import name.benjaminabbitt.evented.projector.ProjectorGrpc.ProjectorImplBase;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-public class BookmarkProjector {
-    private static final Logger logger = Logger.getLogger(Projector.BookmarkProjector.class.getName());
+public class ProjectorService {
+    private static final Logger logger = Logger.getLogger(ProjectorService.class.getName());
     private final Server server;
 
-    public BookmarkProjector(int port) {
-        this(ServerBuilder.forPort(port));
+    public ProjectorService(int port, MongoClient mongoClient) {
+        this(ServerBuilder.forPort(port), mongoClient);
     }
 
-    public BookmarkProjector(ServerBuilder<?> serverBuilder) {
-        this.server = serverBuilder.addService(new BookmarkService()).build();
+    public ProjectorService(ServerBuilder<?> serverBuilder, MongoClient mongoClient) {
+        this.server = serverBuilder.addService(new BookmarkService(new BookmarkProjector(mongoClient))).build();
     }
 
     public void start() throws IOException {
@@ -35,7 +34,7 @@ public class BookmarkProjector {
                 // Use stderr here since the logger may have been reset by its JVM shutdown hook.
                 System.err.println("*** shutting down gRPC server since JVM is shutting down");
                 try {
-                    Projector.BookmarkProjector.this.stop();
+                    ProjectorService.this.stop();
                 } catch (InterruptedException e) {
                     e.printStackTrace(System.err);
                 }
@@ -63,8 +62,10 @@ public class BookmarkProjector {
     }
 
     private static class BookmarkService extends ProjectorImplBase {
+        private final EventPageDispatcher dispatcher;
 
-        public BookmarkService() {
+        public BookmarkService(EventPageDispatcher dispatcher) {
+            this.dispatcher = dispatcher;
         }
 
 
@@ -74,7 +75,10 @@ public class BookmarkProjector {
          */
         @Override
         public void handleSync(Evented.EventBook request, StreamObserver<Evented.Projection> responseObserver) {
-            super.handleSync(request, responseObserver);
+            Evented.Cover cover = request.getCover();
+            request.getPagesList().forEach(ea -> {
+                dispatcher.dispatch(cover, ea);
+            });
         }
 
         /**
@@ -88,7 +92,8 @@ public class BookmarkProjector {
     }
 
     public static void main(String[] args) throws Exception {
-        Projector.BookmarkProjector server = new Projector.BookmarkProjector(8080);
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        ProjectorService server = new ProjectorService(8080, mongoClient);
         server.start();
         server.blockUntilShutdown();
     }
